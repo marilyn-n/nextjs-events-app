5fe325682dd14f954aedda25f0aaeaa670819ad6
marilyn-n <negrete.aguilar.m@gmail.com>
2021-07-23 17:10:22 -0500

    create layout component

---
+++ src/components/layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MainHeader from './MainHeader';
+
+export default function Layout(props) {
+    return (
+        <>
+        <MainHeader></MainHeader>
+        <main>
+            {props.children}
+        </main>
+        </>
+    );
+}
